c97632a1db26fb1d00055c48362c686f194fa707
Jun662255 <103052354+Jun662255@users.noreply.github.com>
2023-01-06 17:58:23 +0900

    c

---
+++ foProject/src/main/java/com/ta9/FoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ta9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoProjectApplication.class, args);
+	}
+
+}
---
+++ foProject/src/test/java/com/ta9/FoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ta9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
